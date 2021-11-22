71d24c44f8b8d67f512695ae7cad244321b02670
PFZM <pfzm@hotmail.com>
2021-11-22 22:17:50 +1100

    added get/:id in the 3 routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: Tag }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
